3d6b52944fa3747fd6f1416ff4e9aff9628dbd27
kevin <onedayatatime0923@gmail.com>
2018-10-06 20:53:28 +0800

    all

---
+++ test/src/main/insertionMain.cpp
@@ -0,0 +1,32 @@
+
+#ifdef SORT_INSERTION
+#include <iostream>
+#include <stdio.h>
+#include "../util/database.h"
+#include "../sorter/insertionSort.h"
+
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+  AlgTimer t;
+  t.Begin();
+
+  DataBase database(argv[1]);
+  InsertionSorter sorter;
+
+  int * order = sorter.sort(database);
+
+  // cout<< database.getStringSize()<<endl;
+  // for(int i = 0; i<database.getStringSize(); ++i){
+  //   cout<< database(order[i]) << endl;
+  // }
+  database.write(order, argv[2]);
+  // Display the accumulated time
+  cout << "The execution spends " << t.End() << " seconds" << endl;
+
+
+  return 0;
+  
+}
+#endif
