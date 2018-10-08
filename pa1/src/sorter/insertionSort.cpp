01fef6e4d6a8e5b28f359ca90d6d1505862ac4f6
kevin <onedayatatime0923@gmail.com>
2018-10-08 16:00:18 +0800

    all

--- pa1/src/sorter/insertionSort.cpp
+++ pa1/src/sorter/insertionSort.cpp
@@ -7,8 +7,13 @@ int* InsertionSorter::sort(DataBase& database){
   order[0] = 0;
   for(int i = 1;i < size; ++i){
     int j;
-    for(j = i - 1; j >= 0 && database.largerthan(order[j],i); --j){
-      order[j+1] = order[j];
+    for(j = i - 1; j >= 0; --j){
+      if( database.largerthan(order[j],i)){
+        order[j+1] = order[j];
+      }
+      else{
+        break;
+      }
     };
     order[j+1] = i;
   }
