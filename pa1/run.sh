fbd28fe2966f5fb4ea92808c2005e024509efd6e
onedayatatime0923 <onedayatatime0923@gmail.com>
2018-10-12 21:21:42 +0800

    all

--- pa1/run.sh
+++ pa1/run.sh
@@ -1,11 +1,11 @@
 
 export exe=${1:-time}
 export output=${2:-output}
-${exe} ./insertionSort.bin data/case4.dat ${output}_insertion
+time ./insertionSort.bin data/case4.dat ${output}_insertion
 echo ""
-${exe} ./mergeSort.bin data/case4.dat ${output}_merge
+time ./mergeSort.bin data/case4.dat ${output}_merge
 echo ""
-${exe} ./heapSort.bin data/case4.dat ${output}_heap
+time ./heapSort.bin data/case4.dat ${output}_heap
 echo ""
-${exe} ./quickSort.bin data/case4.dat ${output}_quick
+time ./quickSort.bin data/case4.dat ${output}_quick
 
