99fc1e5ae36206664ca319433296b80a557ae53c
kevin <onedayatatime0923@gmail.com>
2018-10-12 20:49:53 +0800

    all

---
+++ pa1/build/CMakeFiles/h.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h.dir/src/main/heapMain.cpp.o"
+  "../heapSort.bin.pdb"
+  "../heapSort.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
