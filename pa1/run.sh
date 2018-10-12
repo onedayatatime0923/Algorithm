831ff885d43863852b3fb586235be2923916cc55
kevin <onedayatatime0923@gmail.com>
2018-10-12 21:11:54 +0800

    all

--- pa1/run.sh
+++ pa1/run.sh
@@ -1,11 +1,11 @@
 
 export exe=${1:-time}
 export output=${2:-output}
-$1 ./insertionSort.bin data/case4.dat ${output}_insertion
+${exe} ./insertionSort.bin data/case4.dat ${output}_insertion
 echo ""
-$exe ./mergeSort.bin data/case4.dat ${output}_merge
+${exe} ./mergeSort.bin data/case4.dat ${output}_merge
 echo ""
-$exe ./heapSort.bin data/case4.dat ${output}_heap
+${exe} ./heapSort.bin data/case4.dat ${output}_heap
 echo ""
-$exe ./quickSort.bin data/case4.dat ${output}_quick
+${exe} ./quickSort.bin data/case4.dat ${output}_quick
 
