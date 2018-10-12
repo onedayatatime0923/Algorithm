93128f7de240611116fae2c9767626a59766333e
kevin <onedayatatime0923@gmail.com>
2018-10-12 21:02:00 +0800

    all

--- pa1/run.sh
+++ pa1/run.sh
@@ -1,12 +1,11 @@
 
-cd build
-cmake ..
-make
-cd ..
 export exe=${1:-time}
 export output=${2:-output}
 $1 ./insertionSort.bin data/case4.dat ${output}_insertion
-$1 ./mergeSort.bin data/case4.dat ${output}_merge
-$1 ./heapSort.bin data/case4.dat ${output}_heap
-$1 ./quickSort.bin data/case4.dat ${output}_quick
+echo ""
+$exe ./mergeSort.bin data/case4.dat ${output}_merge
+echo ""
+$exe ./heapSort.bin data/case4.dat ${output}_heap
+echo ""
+$exe ./quickSort.bin data/case4.dat ${output}_quick
 
