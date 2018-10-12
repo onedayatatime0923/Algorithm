e3eeea84a6fe33fea83393af6c87e72aab0f36c2
kevin <onedayatatime0923@gmail.com>
2018-10-12 15:58:07 +0800

    all

--- pa1/run.sh
+++ pa1/run.sh
@@ -1,10 +1,12 @@
 
 cd build
+cmake ..
 make
 cd ..
-export output=${1:-output}
-./insertionSort data/case4.dat ${output}_insertion
-./mergeSort data/case4.dat ${output}_merge
-./heapSort data/case4.dat ${output}_heap
-./quickSort data/case4.dat ${output}_quick
+export exe=${1:-time}
+export output=${2:-output}
+$1 ./insertionSort data/case4.dat ${output}_insertion
+$1 ./mergeSort data/case4.dat ${output}_merge
+$1 ./heapSort data/case4.dat ${output}_heap
+$1 ./quickSort data/case4.dat ${output}_quick
 
