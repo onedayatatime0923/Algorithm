8243da034020d7a66c2413929a350c19709a9559
kevin <onedayatatime0923@gmail.com>
2018-11-15 21:44:59 +0800

    all

---
+++ pa2/build/CMakeFiles/SRCLIB.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SRCLIB.dir/src/dp/dp.cpp.o"
+  "CMakeFiles/SRCLIB.dir/src/io/io.cpp.o"
+  "CMakeFiles/SRCLIB.dir/src/util/chordTable.cpp.o"
+  "CMakeFiles/SRCLIB.dir/src/util/chordVec.cpp.o"
+  "CMakeFiles/SRCLIB.dir/src/util/table.cpp.o"
+  "../lib/libSRCLIB.pdb"
+  "../lib/libSRCLIB.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SRCLIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
